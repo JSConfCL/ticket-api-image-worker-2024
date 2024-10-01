9523844e8a0e9d005ba6565f7c9e2d5a579c72d7
Jose Lezama <me@joseglego.io>
2024-09-30 22:46:58 -0300

    feat: create ticket for 2024

--- src/index.ts
+++ src/index.ts
@@ -11,8 +11,96 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
+import { ImageResponse, loadGoogleFont } from "workers-og";
+import { corsHeaders } from "./cors";
+import { template } from "./template";
+
+export interface Env {
+  OG_BUCKET: R2Bucket;
+  BUCKET_AUTH_KEY_SECRET: string;
+  API_URL: string;
+}
+
+export const PATTERNS = {
+  bucket: new URLPattern({ pathname: "/bucket/:key" }),
+};
+
+interface PublicTicketInfo {
+  id: string;
+  userName: string;
+  userUsername: string;
+}
+
+const getApiInformation = async (URL: string, ticketId: string) => {
+  const headers = new Headers();
+  headers.append("Content-Type", "text/javascript");
+
+  console.log(`Fetching API information for ticket ${ticketId} in ${URL}`);
+
+  try {
+    const response  = await fetch(URL, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        query: `query PublicTicketInfo ($publicTicketId: String!) { publicTicketInfo (input: { publicTicketId: $publicTicketId } ) { id userName userUsername } }`,
+        variables: { publicTicketId: ticketId },
+      }),
+    });
+    console.log(`Parsing Data`);
+    const baseData: { data: { publicTicketInfo: PublicTicketInfo } } = await response.json();
+    const publicTicketInfo: PublicTicketInfo = baseData.data.publicTicketInfo;
+    console.log(`Returning Data`);
+    return {
+      name: publicTicketInfo?.userName ?? "",
+      username: publicTicketInfo?.userUsername ?? ""
+    };
+  } catch (error) {
+    throw new Error(`Failed to fetch API information for ticket ${ticketId} in ${URL}`);
+  }
+};
+
 export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
-	},
-} satisfies ExportedHandler<Env>;
+  async fetch(
+    request: Request,
+    env: Env,
+    _ctx: ExecutionContext
+  ): Promise<Response> {
+    const params = new URL(request.url).searchParams;
+
+    const format = params.get("format");
+    const debug = params.get("debug");
+    const ticketId = params.get("ticketId");
+    const url = env.API_URL;
+
+    if (!ticketId) {
+      throw new Error("no ticket");
+    }
+
+    const data = await getApiInformation(url, ticketId);
+    const element = template({
+      name: data.name, username: data.username
+    });
+
+    return new ImageResponse(element, {
+      format: format as "svg" | "png",
+      debug: debug === "true",
+      fonts: [
+        {
+          name: "Barlow",
+          data: await loadGoogleFont({ family: "Barlow", weight: 600 }),
+          weight: 600,
+          style: "normal",
+        },
+        {
+          name: "Inconsolata",
+          data: await loadGoogleFont({ family: "Inconsolata", weight: 700 }),
+          weight: 700,
+          style: "normal",
+        },
+      ],
+      headers: {
+        ...corsHeaders
+      },
+    });
+  },
+};
